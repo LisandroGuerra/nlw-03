17b60a374da6d8c07c79f253c5abce867c751015
LisandroGuerra <lisandro.digital@gmail.com>
2020-10-12 11:45:36 -0300

    Importing landing CSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import './syles/global.css';
+import './styles/global.css';
+import './styles/pages/landing.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <div id="page-landing">
       <h1>Hello World!!!</h1>
     </div>
   );
