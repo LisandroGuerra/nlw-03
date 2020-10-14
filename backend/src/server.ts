03ac6cd81b539ae07942831eaaf1fc4b69433af0
LisandroGuerra <lisandro.digital@gmail.com>
2020-10-13 23:58:01 -0300

    Create migrations directory and edit ormconfig file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import express, { request, response } from 'express';
 
+import './database/connection';
+
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World!' });
