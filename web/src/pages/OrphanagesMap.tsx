c5baf329f6d2fb06d2ea094690aa5e486ed0ac62
LisandroGuerra <lisandro.digital@gmail.com>
2020-10-17 23:31:58 -0300

    Including marker on map anda more two pages

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 4]
+});
 
 function OrphanagesMap() {
     return (
@@ -28,10 +37,28 @@ function OrphanagesMap() {
             </aside>
 
            <Map center={[-30.040694,-51.1283178]}
-            zoom={45}
+            zoom={75}
             style={{ width: '100%', height: '100%'}}
             >
                  <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                 <Marker 
+                    icon={mapIcon} 
+                    position={[-30.040694,-51.1261291]}
+                 >
+                      
+                     <Popup className="map-popup" closeButton={false} minWidth={240} maxWidth={240}>
+                        Nome do local
+                         <Link to="">                            
+                             <FiArrowRight size={20} color="#ffffff" />
+                         </Link>
+
+                     </Popup>
+
+                 </Marker>
+
+
+
            </Map>
             
               
