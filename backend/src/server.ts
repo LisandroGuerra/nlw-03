d3a1ab379017c4b2f73556ef587b70584716f69d
LisandroGuerra <lisandro.digital@gmail.com>
2020-10-17 21:55:38 -0300

    Create errors directory, handler file and handling errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express, { request, response } from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 
 import './database/connection';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 
 const app = express();
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 
 app.listen(3333);
